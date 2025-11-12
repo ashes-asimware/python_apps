60a36dfdaa4198802161da8f4529e6dba50e2838
ashes-asimware <103869509+ashes-asimware@users.noreply.github.com>
2025-11-12 15:12:33 -0800

    Commit 11122025151200

--- python-training/training/datascience/main.py
+++ python-training/training/datascience/main.py
@@ -16,24 +16,34 @@ df = pd.DataFrame(data)
 # use iloc to get the same result
 # print(df.iloc[[0, 2], [0, 2]])
 
-
-fruits = ["apple", "banana", "cherry", "date"]
-# use this list and create a random sample of 50 fruits using choice
-sampled_fruits = random.choices(fruits, k=50)
-# print(sampled_fruits)
-
-# create a dictionary with product names, quantity and prices
-# using the sampled_fruits list, create a dataframe with random quantities and prices for each fruit
-# add category column with random categories from a list of fruits and vegetables
-product_data = {
-    "product": sampled_fruits,
-    "category": [random.choice(["fruit", "vegetable"]) for _ in range(50)],
-    "quantity": [random.randint(1, 10) for _ in range(50)],
-    "amount": [round(random.uniform(0.5, 5.0), 2) for _ in range(50)],
+sample_size = 20
+products = [
+    "Apple (F)",
+    "Banana (F)",
+    "Carrot (V)",
+    "Broccoli (V)",
+    "Chicken (P)",
+    "Fish (P)",
+]
+categories = {"F": "Fruit", "V": "Vegetable", "P": "Protein"}
+# Create list of 50 products by randomly selecting from products
+product_list = [random.choice(products) for _ in range(sample_size)]
+# create dictionary with 3 keys - "Product", "Category", "Quantity"
+# values of "Product" should be name without the category suffix
+# values of "Category" should be full category name from categories dict
+# values of "Quantity" should be random integer between 1 and 100
+data = {
+    "Product": [p.split(" (")[0] for p in product_list],
+    "Category": [categories[p.split(" (")[1][0]] for p in product_list],
+    "Quantity": [random.randint(1, 100) for _ in range(sample_size)],
 }
-df_products = pd.DataFrame(product_data)
+df_products = pd.DataFrame(data)
+print(df_products)
 
 pivot = df_products.pivot_table(
-    index="category", values="amount", aggfunc=["mean", "median", "min"]
+    index="Category",
+    values="Quantity",
+    aggfunc=["sum", "mean", "max"],
+    fill_value=-1,
 )
 print(pivot)
