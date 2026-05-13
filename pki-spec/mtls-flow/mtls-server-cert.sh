44a4d27995ae7f9f8b442e0536fdc447a1ca5717
Ashes Mukherjee <103869509+ashes-asimware@users.noreply.github.com>
2026-05-13 11:37:56 -0700

    Add script to generate CA and server certificates
    This script generates a Certificate Authority (CA) and a server certificate using OpenSSL. It creates a directory for certificates, generates a CA key and certificate, a server key and certificate signing request (CSR), and finally signs the server certificate with the CA.

--- pki-spec/mtls-flow/mtls-server-cert.sh
+++ pki-spec/mtls-flow/mtls-server-cert.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+set -e
 
+mkdir -p certs
+cd certs
+
+echo "== Creating CA =="
+openssl genrsa -out ca.key 2048
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 365 \
+  -subj "/C=US/ST=CA/O=DemoCA/CN=Demo-CA" \
+  -out ca.crt
+
+echo "== Creating Server Key & CSR =="
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key \
+  -subj "/C=US/ST=CA/O=DemoServer/CN=localhost" \
+  -out server.csr
+
+echo "== Signing Server Cert with CA =="
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial \
+  -out server.crt -days 365 -sha256
+
+echo "Server certs generated in ./certs"
+``
